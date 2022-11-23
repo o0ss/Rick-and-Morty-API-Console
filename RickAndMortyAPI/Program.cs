1556312c081931074587810beed559d77a5d0731
sixtos04@gmail.com <sixtos04@gmail.com>
2022-11-22 18:27:25 -0600

    Add project files.

---
+++ RickAndMortyAPI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+
+namespace RickAndMortyAPI
+{
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+        static void Main()
+        {
+            Console.WriteLine("Rick & Morty API");
+
+            client.BaseAddress = new Uri("https://rickandmortyapi.com/api");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json")
+                );
+        }
+
+        static async Task<bool> ObtenPersonaje()
+        {
+            HttpResponseMessage response = await client.GetAsync("character/2");
+        }
+    }
+}
